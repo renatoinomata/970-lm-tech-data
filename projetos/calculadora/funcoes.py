ed19e819010d826221e26e9a4673f86cd276175f
Renato Inomata <renato.inomata@gmail.com>
2023-04-01 09:33:20 -0300

    Readequando função de soma

--- projetos/calculadora/funcoes.py
+++ projetos/calculadora/funcoes.py
@@ -1,11 +1,10 @@
 # Arquivo de funções:
 
 def soma(a, b):
-    try:
-        soma = a + b
-        return soma
-    except TypeError:
-        print(f'O input "a" e "b" devem ser numéricos, recebido a = {a}, tipo = {type(a)} e b = {b}, tipo = {type(b)}')
+    if isinstance(a, (int, float)) and isinstance(b, (int, float)):
+        return a + b
+    else:
+        raise TypeError(f'O input "a" e "b" devem ser numéricos, recebido a = {a}, tipo = {type(a)} e b = {b}, tipo = {type(b)}')
     
 def subtracao(a, b):
     if isinstance(a, (int, float)) and isinstance(b, (int, float)):
