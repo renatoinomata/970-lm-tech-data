a925a2a3517480707c60ffce40134b93fccc1239
Renato Inomata <renato.inomata@gmail.com>
2023-04-01 21:02:35 -0300

    Adicionando função de divisão

--- projetos/calculadora/funcoes.py
+++ projetos/calculadora/funcoes.py
@@ -12,3 +12,11 @@ def subtracao(a, b):
     else:
         raise TypeError(f'O input "a" e "b" devem ser numéricos, recebido a = {a}, tipo = {type(a)} e b = {b}, tipo = {type(b)}')
     
+def divisao(a, b):
+    if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
+        raise TypeError(f'O input "a" e "b" devem ser numéricos, recebido a = {a}, tipo = {type(a)} e b = {b}, tipo = {type(b)}')  
+    
+    if b != 0:
+        return a / b
+    else:
+        raise ZeroDivisionError(f'O input "b" deve ser diferente de zero')
