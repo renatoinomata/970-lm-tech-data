9a17bffec37f8eb0480811a74988fd01f1d4e855
Renato Inomata <renato.inomata@gmail.com>
2023-04-01 21:33:57 -0300

    Adicionando informações em app.py

---
+++ projetos/calculadora/app.py
@@ -0,0 +1,10 @@
+# Importanto a função do arquivo calculadora/__init__.py
+import os
+import sys
+sys.path.insert(0, os.getcwd())
+from projetos.calculadora import calcule
+
+# Executando a aplicação
+calcule()
+
+# Executar o código no terminal com python app.py
